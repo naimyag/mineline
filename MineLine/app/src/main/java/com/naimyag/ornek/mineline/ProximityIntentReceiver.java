36a770916cf82093a4f5b80d357785d0e9a081cb
naimyag <naimyag@gmail.com>
2016-02-03 00:23:13 +0200

    Notifikasyon aktif hale getirildi.

--- MineLine/app/src/main/java/com/naimyag/ornek/mineline/ProximityIntentReceiver.java
+++ MineLine/app/src/main/java/com/naimyag/ornek/mineline/ProximityIntentReceiver.java
@@ -6,9 +6,7 @@ import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Color;
 import android.location.LocationManager;
-import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -16,39 +14,6 @@ import android.widget.Toast;
  * Created by Naim on 31.01.2016.
  */
 public class ProximityIntentReceiver extends BroadcastReceiver {
-
-    Context context;
-
-    private static final int NOTIFICATION_ID = 1000;
-
-
-
-    public void notification(){
-
-        NotificationManager mng =
-                (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-
-        Notification.Builder nb = new Notification.Builder(context);
-
-        // API 21+ olması lazım
-        // nb.setColor(Color.GREEN);
-
-        nb.setContentText("Hoşgeldin Beşgittin!!");
-        nb.setContentTitle("MineLine");
-        nb.setSmallIcon(R.drawable.alerts);
-        nb.setVibrate(new long[] {400L, 400L});
-
-
-        // API 16+ build altında getNotification()
-        // Notification not = nb.build();
-        Notification not = nb.getNotification();
-
-        mng.notify(1, not);
-
-
-    }
-
-
 
 
     @Override
@@ -64,54 +29,82 @@ public class ProximityIntentReceiver extends BroadcastReceiver {
             Toast.makeText(context, "HOŞGELDİN!", Toast.LENGTH_LONG).show();
             Log.d(getClass().getSimpleName(), "entering");
 
-            NotificationManager notificationManager =
-                    (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
 
             PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
 
-            //  Notification notification = createNotification();
+
+            NotificationManager mng =
+                    (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+            Notification.Builder nb = new Notification.Builder(context);
+
+            // API 21+ olması lazım
+            // nb.setColor(Color.GREEN);
+
+            nb.setContentText("Bölgeye Girdin Hafız");
+            nb.setContentTitle("MAYIN");
+            nb.setContentIntent(pendingIntent);
+            nb.setSmallIcon(R.drawable.alerts);
+            nb.setVibrate(new long[] {500L, 500L, 500L, 500L});
 
 
-            Notification notification = new Notification();
+            // API 16+ build altında getNotification()
+            // Notification not = nb.build();
+            Notification not = nb.getNotification();
 
-            notification.icon = R.drawable.ic_menu_notifications;
-            notification.when = System.currentTimeMillis();
-
-            notification.flags |= Notification.FLAG_AUTO_CANCEL;
-            notification.flags |= Notification.FLAG_SHOW_LIGHTS;
-
-            notification.defaults |= Notification.DEFAULT_VIBRATE;
-            notification.defaults |= Notification.DEFAULT_LIGHTS;
-
-            notification.ledARGB = Color.WHITE;
-            notification.ledOnMS = 1500;
-            notification.ledOffMS = 1500;
+            mng.notify(1, not);
 
 
 
 
+            //    NotificationManager notificationManager =
+       //            (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+       //    PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+
+       //    //  Notification notification = createNotification();
 
 
-            //  notification.setLatestEventInfo(context, "Proximity Alert!", "You are near your point of interest.", pendingIntent);
+       //    Notification notification = new Notification();
 
-            Notification.Builder builder = new Notification.Builder(context);
+       //    notification.icon = R.drawable.ic_menu_notifications;
+       //    notification.when = System.currentTimeMillis();
 
+       //    notification.flags |= Notification.FLAG_AUTO_CANCEL;
+       //    notification.flags |= Notification.FLAG_SHOW_LIGHTS;
 
-            //builder.setAutoCancel(false);
-            //builder.setTicker("this is ticker text");
-            builder.setContentTitle("Proximity Alert!");
-            builder.setContentText("You are near your point of interest.");
-            //builder.setSmallIcon(R.drawable.ic_launcher);
-            builder.setContentIntent(pendingIntent);
-            //builder.setOngoing(true);
-            //builder.setSubText("This is subtext...");   //API level 16
-            //builder.setNumber(100);
-            builder.build();
+       //    notification.defaults |= Notification.DEFAULT_VIBRATE;
+       //    notification.defaults |= Notification.DEFAULT_LIGHTS;
 
-            notification = builder.getNotification();
+       //    notification.ledARGB = Color.WHITE;
+       //    notification.ledOnMS = 1500;
+       //    notification.ledOffMS = 1500;
 
 
-            notificationManager.notify(NOTIFICATION_ID, notification);
+
+
+
+
+       //    //  notification.setLatestEventInfo(context, "Proximity Alert!", "You are near your point of interest.", pendingIntent);
+
+       //    Notification.Builder builder = new Notification.Builder(context);
+
+
+       //    //builder.setAutoCancel(false);
+       //    //builder.setTicker("this is ticker text");
+       //    builder.setContentTitle("Proximity Alert!");
+       //    builder.setContentText("You are near your point of interest.");
+       //    //builder.setSmallIcon(R.drawable.ic_launcher);
+       //    builder.setContentIntent(pendingIntent);
+       //    //builder.setOngoing(true);
+       //    //builder.setSubText("This is subtext...");   //API level 16
+       //    //builder.setNumber(100);
+       //    builder.build();
+
+       //    notification = builder.getNotification();
+
+
+       //    notificationManager.notify(NOTIFICATION_ID, notification);
 
         }
         else {
